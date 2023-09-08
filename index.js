d2ebfd14fd6cde2c63279f9c6f55fb5e4865c75b
Dávid Godzsák <godzsakdavid@gmail.com>
2023-09-08 15:57:01 +0200

    upload images, delete files

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const workshopRouter = require('./routes/workshop.js');
+const uploadRouter = require('./routes/uploader.js');
 const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cors({
+    allowedHeaders: ["GET", "POST", "PUT", "DELETE"],
+    origin: ["http://localhost:5174", "https://henger.studio", "https://admin.henger.studio"]
+}));
 
 // routes
 app.use('/api/workshop', workshopRouter);
+app.use('/api/upload', uploadRouter);
     
 // start server
+console.log('Starting on port 3000')
 app.listen(process.env.PORT || 3000);
--- routes/workshop.js
+++ routes/workshop.js
@@ -1,9 +1,70 @@
-const express = require('express')
-const router = express.Router()
+'use strict';
 
-router.get('/', (req, res) => {
-    console.log(req.body)
-    res.json(req.body)
+const express = require('express');
+const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { randomUUID } = require('crypto');
+const multer = require("multer");
+const sharp = require("sharp");
+const fs = require("fs");
+
+const router = express.Router();
+const client = new S3Client();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const DB_PATH = 'db';
+const UPLOAD_PATH = 'upload';
+const HENGER_URL = 'https://henger.studio'
+
+const workshopS3Request = { Bucket: BUCKET_NAME, Key: `${DB_PATH}/workshops.json` };
+const imageS3Request = (filename, body) => ({ Bucket: BUCKET_NAME, Key: `${UPLOAD_PATH}/${filename}`, Body: body });
+
+// todo use direct read of workshops file in FE
+router.get('/', async (req, res) => {
+    const workshops = await getWorkshopsFromAws()
+        .catch(e => res.status(500).json({ message: "AWS Error", error: e }));
+
+    // filter for only relevant ones
+    // workshops.filter();
+
+    return res.json(workshops);
 })
+
+// todo protect
+router.post('/', async (req, res) => {
+    console.log(req.body);
+
+    // validate if they sent a workshop
+    if (!isWorkshop(req.body)) {
+        return res.status(400).json({ message: "Not a workshop" });
+    }
+
+    const workshops = await getWorkshopsFromAws()
+        .catch(e => res.status(500).json({ message: "AWS Error", error: e }));
+
+    const command = PutObjectCommand({ Body: [...workshops, req.body], ...workshopS3Request });
+    await client.send(command)
+        .catch(e => res.status(500).json({ message: "Could not save the image!", error: e }));
+
+    res.status(201).json({ message: "Added workshop!" })
+})
+
+// helpers
+function isWorkshop(request) {
+    const hasTitle = "";
+    return hasTitle;
+}
+
+async function getWorkshopsFromAws() {
+    const command = new GetObjectCommand(workshopS3Request);
+    return await client.send(command)
+        .then(async (it) => await readBodyToJson(it))
+}
+
+function readBodyToJson(it) {
+    return it.Body.transformToString().then(b => JSON.parse(b));
+}
 
 module.exports = router
