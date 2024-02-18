95c0a1b0a920ba5d438865bd32a833738d51f2f0
Dávid Godzsák <godzsakdavid@gmail.com>
2024-02-18 14:59:30 +0100

    Fix cors

--- index.mjs
+++ index.mjs
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({
     allowedHeaders: ["Content-Type"],
-    origin: ["http://localhost:5173", "https://henger.studio", "https://admin.henger.studio"]
+    origin: ["http://localhost:5173", "https://henger.studio", "https://content.henger.studio"]
 }));
 
 // routes
